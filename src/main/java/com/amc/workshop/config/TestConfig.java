c86e22ea9cc2b1cc1ae1f25e65c75d45294fcf43
Arthur Carreiro <167469828+ArtCarreiro@users.noreply.github.com>
2024-09-04 15:42:39 -0300

    Add OrderStatus enum

--- src/main/java/com/amc/workshop/config/TestConfig.java
+++ src/main/java/com/amc/workshop/config/TestConfig.java
@@ -9,6 +9,7 @@ import java.time.Instant;
 
 import com.amc.workshop.entities.Order;
 import com.amc.workshop.entities.User;
+import com.amc.workshop.entities.enums.OrderStatus;
 import com.amc.workshop.repositories.OrderRepository;
 import com.amc.workshop.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User us2 = new User(null, "Pedro", "Pedrodb@gmail.com", "314791617", "senhadopedro");
         User us3 = new User(null, "Joana", "Joanadb@gmail.com", "318167319", "senhadaJoana");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), us1); 
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), us2); 
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), us3);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.DELIVERED , us1); 
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.PAID ,us2); 
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT ,us3);
 
         userRepository.saveAll(Arrays.asList(us1, us2, us3));
 
--- src/main/java/com/amc/workshop/entities/Order.java
+++ src/main/java/com/amc/workshop/entities/Order.java
@@ -3,6 +3,7 @@ package com.amc.workshop.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.amc.workshop.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -28,12 +29,25 @@ private static final long serialVersionUID = 1L;
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus ,User client) {
         Id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public Long getId() {
