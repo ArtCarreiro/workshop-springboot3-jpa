4408da417cbc3d2397ce2f603b41057e4259c38e
Arthur Carreiro <167469828+ArtCarreiro@users.noreply.github.com>
2024-09-10 13:51:49 -0300

    delete

--- src/main/java/com/amc/workshop/resources/UserResource.java
+++ src/main/java/com/amc/workshop/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,8 +40,14 @@ public class UserResource {
     public ResponseEntity<User> createUser(@RequestBody User obj) {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-        .path("/{id}").buildAndExpand(obj.getId()).toUri();
+                .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
