c2aeade35eb883a3cbe953f9af7bac49786f77be
Arthur Carreiro <167469828+ArtCarreiro@users.noreply.github.com>
2024-09-11 15:35:45 -0300

    exception DB

--- src/main/java/com/amc/workshop/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/amc/workshop/resources/exceptions/ResourceExceptionHandler.java
@@ -1,4 +1,5 @@
 package com.amc.workshop.resources.exceptions;
+
 import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
@@ -6,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.amc.workshop.services.exceptions.DataBaseException;
 import com.amc.workshop.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,11 +16,18 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 
   @ExceptionHandler(ResourceNotFoundException.class)
-  public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
-      String error = "Resource not found";
-      HttpStatus status = HttpStatus.NOT_FOUND;
-      StandardError sr = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
-      return ResponseEntity.status(status).body(sr);
+  public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+    String error = "Resource not found";
+    HttpStatus status = HttpStatus.NOT_FOUND;
+    StandardError sr = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+    return ResponseEntity.status(status).body(sr);
   }
 
+  @ExceptionHandler(DataBaseException.class)
+  public ResponseEntity<StandardError> databaseException(DataBaseException e, HttpServletRequest request) {
+    String error = "DataBase error";
+    HttpStatus status = HttpStatus.BAD_REQUEST;
+    StandardError bdsr = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+    return ResponseEntity.status(status).body(bdsr);
+  }
 }
--- src/main/java/com/amc/workshop/services/UserService.java
+++ src/main/java/com/amc/workshop/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.amc.workshop.entities.User;
 import com.amc.workshop.repositories.UserRepository;
+import com.amc.workshop.services.exceptions.DataBaseException;
 import com.amc.workshop.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,11 +32,19 @@ public class UserService {
         return repository.save(obj);
     }
 
-    public void delete(Long id){
-        repository.deleteById(id);
+    public void delete(Long id) {
+
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException(e.getMessage());
+        }
+
     }
 
-    public User update(Long id, User obj){
+    public User update(Long id, User obj) {
         User entity = repository.getReferenceById(id);
         updateUser(entity, obj);
         return repository.save(entity);
@@ -44,7 +55,5 @@ public class UserService {
         entity.setName(obj.getName());
         entity.setPhone(obj.getPhone());
     }
-
-    
 
 }
